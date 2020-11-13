66324c03f2a51966d4da66e91759dd59fbe7ba47
Ariya Ikhwanu <ariyaikhwanu@gmail.com>
2020-11-13 11:36:41 +0700

    game suit jawa dengan java script

--- README.txt
+++ README.txt
@@ -4,3 +4,7 @@ jenis pop up box
 1.alert   : Untuk informasi pengguna
 2.prompt  : Dapat mengembalikan nilai masukan
 3.confirm : bisa kembalikan data tapi tidak ada isi, hanya ya/tidak( mengembalikan true/false aja)
+
+
+fungsi
+parseInt (mengubah menjadi integer)
--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-var x = 10;
-console.log(x);
-console.log("Hello world");
+// var x = 10;
+// console.log(x);
+// console.log("Hello world");
 
-//alert('Hello World!');
+// alert('Hello World!');
 
 //var nama = prompt('Masukan Nama: ');
 //console.log(nama);
@@ -14,14 +14,62 @@ console.log("Hello world");
 //   alert("User pencet cancel!");
 // }
 
-alert("Selamat Datang..");
-var lagi = true;
+// alert("Selamat Datang..");
+// var lagi = true;
 
-while (lagi === true) {
-  var nama = prompt("Masukan nama: ");
-  alert("Hallo " + nama);
+// while (lagi === true) {
+//   var nama = prompt("Masukan nama: ");
+//   alert("Hallo " + nama);
 
-  lagi = confirm("Coba lagi ?");
+//   lagi = confirm("Coba lagi ?");
+// }
+
+// alert("Terima kasih..");
+tanya = true;
+while (tanya == true) {
+  var p = prompt("Silakan pilih : gajah, semut, atau orang");
+  var computer = Math.random();
+
+  if (computer < 0.34) {
+    computer = "gajah";
+  } else if (computer >= 0.34 && computer < 0.67) {
+    computer = "semut";
+  } else {
+    computer = "orang";
+  }
+
+  var hasil = "";
+
+  if (p == computer) {
+    hasil = "SERI!";
+  } else if (p == "gajah") {
+    //   if (computer == "orang") {
+    //     hasil = "Menang";
+    //   } else {
+    //     hasil = "Kalah";
+    //   }
+    hasil = computer == "orang" ? "Menang" : "Kalah";
+  } else if (p == "orang") {
+    if (computer == "gajah") {
+      hasil = "Menang";
+    } else {
+      hasil = "Kalah";
+    }
+  } else if (p == "semut") {
+    hasil = computer == "orang" ? "Kalah" : "Menang";
+  } else {
+    hasil = "Memasukkan pilihan yang salah";
+  }
+
+  alert(
+    "Kamu pilih: " +
+      p +
+      " dan Komputer memilih : " +
+      computer +
+      " \n maka hasilnya kamu: " +
+      hasil
+  );
+  tanya = confirm("Apakah ingin lanjut?");
 }
 
-alert("Terima kasih..");
+alert("Terima kasih telah bermain");
