02ade70ec047ddfe4224af0f19f7691763c98a33
Ariya Ikhwanu <ariyaikhwanu@gmail.com>
2020-11-13 15:41:43 +0700

    Bikin game tebak angka

--- script.js
+++ script.js
@@ -1,3 +1,106 @@
+var x = 10;
+console.log(x);
+console.log("Hello world");
+
+// alert('Hello World!');
+
+//var nama = prompt('Masukan Nama: ');
+//console.log(nama);
+
+//var tes = confirm("Kamu yakin?");
+//if (tes == true) {
+//   alert("User pencet OK!");
+// } else {
+//   alert("User pencet cancel!");
+// }
+
+// alert("Selamat Datang..");
+// var lagi = true;
+
+// while (lagi === true) {
+//   var nama = prompt("Masukan nama: ");
+//   alert("Hallo " + nama);
+
+//   lagi = confirm("Coba lagi ?");
+// }
+
+// alert("Terima kasih..");
+// tanya = true;
+// while (tanya == true) {
+//   var p = prompt("Silakan pilih : gajah, semut, atau orang");
+//   var computer = Math.random();
+
+//   if (computer < 0.34) {
+//     computer = "gajah";
+//   } else if (computer >= 0.34 && computer < 0.67) {
+//     computer = "semut";
+//   } else {
+//     computer = "orang";
+//   }
+
+//   var hasil = "";
+
+//   if (p == computer) {
+//     hasil = "SERI!";
+//   } else if (p == "gajah") {
+//     //   if (computer == "orang") {
+//     //     hasil = "Menang";
+//     //   } else {
+//     //     hasil = "Kalah";
+//     //   }
+//     hasil = computer == "orang" ? "Menang" : "Kalah";
+//   } else if (p == "orang") {
+//     if (computer == "gajah") {
+//       hasil = "Menang";
+//     } else {
+//       hasil = "Kalah";
+//     }
+//   } else if (p == "semut") {
+//     hasil = computer == "orang" ? "Kalah" : "Menang";
+//   } else {
+//     hasil = "Memasukkan pilihan yang salah";
+//   }
+
+//   alert(
+//     "Kamu pilih: " +
+//       p +
+//       " dan Komputer memilih : " +
+//       computer +
+//       " \n maka hasilnya kamu: " +
+//       hasil
+//   );
+//   tanya = confirm("Apakah ingin lanjut?");
+// }
+
+// alert("Terima kasih telah bermain");
+
+// var tanya = true;
+// var kesempatan = 3;
+
+// while (kesempatan == 0) {
+//   var angka2 = Math.random();
+
+//   var angka = angka2 * 10;
+//   console.log;
+//   var player = prompt(" Silakan tebak angka 1 - 10: ");
+//   if (player == angka) {
+//     alert("Benar! Jawban yang dimaksud adalah angka " + angka);
+//     kesempatan = 0;
+//   } else if (player > angka) {
+//     alert("Salah! jawaban terlalu Rendah, kesempatan tinggal " + kesempatan);
+//     kesempatan = kesempatan - 1;
+//   } else if (player < angka) {
+//     alert("Salah! jawaban terlalu Tinggi, kesempatan tinggal " + kesempatan);
+//     kesempatan = kesempatan - 1;
+//   } else {
+//     alert("input salah");
+//   }
+// }
+//   tanya = confirm("lagi ?");
+// }
+
+// alert("Terima kasih");
+
 // var x = 10;
 // console.log(x);
 // console.log("Hello world");
@@ -25,51 +128,76 @@
 // }
 
 // alert("Terima kasih..");
-tanya = true;
-while (tanya == true) {
-  var p = prompt("Silakan pilih : gajah, semut, atau orang");
-  var computer = Math.random();
+// tanya = true;
+// while (tanya == true) {
+//   var p = prompt("Silakan pilih : gajah, semut, atau orang");
+//   var computer = Math.random();
 
-  if (computer < 0.34) {
-    computer = "gajah";
-  } else if (computer >= 0.34 && computer < 0.67) {
-    computer = "semut";
+//   if (computer < 0.34) {
+//     computer = "gajah";
+//   } else if (computer >= 0.34 && computer < 0.67) {
+//     computer = "semut";
+//   } else {
+//     computer = "orang";
+//   }
+
+//   var hasil = "";
+
+//   if (p == computer) {
+//     hasil = "SERI!";
+//   } else if (p == "gajah") {
+//     //   if (computer == "orang") {
+//     //     hasil = "Menang";
+//     //   } else {
+//     //     hasil = "Kalah";
+//     //   }
+//     hasil = computer == "orang" ? "Menang" : "Kalah";
+//   } else if (p == "orang") {
+//     if (computer == "gajah") {
+//       hasil = "Menang";
+//     } else {
+//       hasil = "Kalah";
+//     }
+//   } else if (p == "semut") {
+//     hasil = computer == "orang" ? "Kalah" : "Menang";
+//   } else {
+//     hasil = "Memasukkan pilihan yang salah";
+//   }
+
+//   alert(
+//     "Kamu pilih: " +
+//       p +
+//       " dan Komputer memilih : " +
+//       computer +
+//       " \n maka hasilnya kamu: " +
+//       hasil
+//   );
+//   tanya = confirm("Apakah ingin lanjut?");
+// }
+
+// alert("Terima kasih telah bermain");
+
+var tanya = true;
+var kesempatan = 3;
+var angka2 = Math.random();
+var angka = parseInt(angka2 * 10);
+console.log(angka);
+while (kesempatan > 0) {
+  var player = prompt(" Silakan tebak angka 0 - 10: ");
+  if (player == angka) {
+    alert("Benar! Jawban yang dimaksud adalah angka " + angka);
+    kesempatan = 0;
+  } else if (player > angka) {
+    kesempatan = kesempatan - 1;
+    alert("Salah! jawaban terlalu Rendah, kesempatan tinggal " + kesempatan);
+  } else if (player < angka) {
+    kesempatan = kesempatan - 1;
+    alert("Salah! jawaban terlalu Tinggi, kesempatan tinggal " + kesempatan);
   } else {
-    computer = "orang";
+    alert("input salah");
   }
-
-  var hasil = "";
-
-  if (p == computer) {
-    hasil = "SERI!";
-  } else if (p == "gajah") {
-    //   if (computer == "orang") {
-    //     hasil = "Menang";
-    //   } else {
-    //     hasil = "Kalah";
-    //   }
-    hasil = computer == "orang" ? "Menang" : "Kalah";
-  } else if (p == "orang") {
-    if (computer == "gajah") {
-      hasil = "Menang";
-    } else {
-      hasil = "Kalah";
-    }
-  } else if (p == "semut") {
-    hasil = computer == "orang" ? "Kalah" : "Menang";
-  } else {
-    hasil = "Memasukkan pilihan yang salah";
-  }
-
-  alert(
-    "Kamu pilih: " +
-      p +
-      " dan Komputer memilih : " +
-      computer +
-      " \n maka hasilnya kamu: " +
-      hasil
-  );
-  tanya = confirm("Apakah ingin lanjut?");
 }
 
-alert("Terima kasih telah bermain");
+// tanya = confirm("lagi ?");
+
+alert("Terima kasih, jawabannya adalah " + angka);
